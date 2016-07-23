2f12bbfcbc1938eecdb19c7c3332dcaa0c353509
Han Yang Tay <htay@wesleyan.edu>
2016-07-23 09:33:40 +0000

    Chapter 5.1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -30,10 +30,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Contact | #{@base_title}"
   end
 
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
-  end
 
 end
